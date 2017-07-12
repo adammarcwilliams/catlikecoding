081053d9abf189f185e68712f8483fb12e5156fc
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2017-07-12 20:23:30 +0100

    Starts Object Pooling tutorial

---
+++ Assets/_Scenes/03_Object_Pools/StuffSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawns;
+    public Stuff[] stuffPrefabs;
+    public float velocityScaler;
+
+    float timeSinceLastSpawn;
+
+    private void FixedUpdate() {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnStuff();
+        }
+    }
+
+    void SpawnStuff () {
+        Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
+        Stuff spawn = Instantiate<Stuff>(prefab);
+        spawn.transform.localPosition = transform.position;
+        spawn.Body.velocity = transform.up * velocityScaler;
+    }
+}
